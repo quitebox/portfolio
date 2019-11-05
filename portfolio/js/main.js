8893ee48e6831838aef4a41db73686c11c34e407
andyelit <47875894+andyelit@users.noreply.github.com>
2019-11-05 08:58:14 +0530

    Games portfolio HTML

---
+++ portfolio/js/main.js
@@ -0,0 +1,13 @@
+var elem = document.getElementById("parent_div_id");
+
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
